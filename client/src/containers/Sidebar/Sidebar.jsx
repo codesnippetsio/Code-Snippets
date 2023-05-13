9854fba859c4ce5ef8b952d06fdd4d077bb84a66
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-05-13 16:33:56 -0500

    refactor getSnippet and renderTabs func

--- client/src/containers/Sidebar/Sidebar.jsx
+++ client/src/containers/Sidebar/Sidebar.jsx
@@ -1,53 +1,66 @@
 import React, {useState, useEffect} from "react";
 import SnippetDisplay from '../../components/SnippetDisplay/SnippetDisplay.jsx';
 import AddSnippet from '../../components/AddSnippet/AddSnippet.jsx';
+import styles from './Sidebar.module.scss'
 
 const Sidebar = () => {
-  const [state, setState] = useState({
-    snippets: [],
-    selectedSnippet: {
-      id: '',
-      title: '',
-      comments: '',
-      storedCode: '',
-      tags: [],
-      language: ''
-    },
-  });
+  const [snippets, setSnippets] = useState([]);
+  const [selectedSnippet, setSelectedSnippet] = useState({});
 
-  // /snippets?id=${_id}
-  // set up getSnippet func
-    // send get request to server
-    // setState with data * deal with CORS warning - cors module & webpack config?
+  // getSnippet func
   const getSnippet = () => {
-    fetch('http://localhost:3000/snippets',{mode: 'no-cors'})
-    .then(res => res) // change back to res.json() *currently encountring error
+    fetch('http://localhost:3000/snippets', {mode: 'no-cors'})
+    .then(res => res.json())
     .then(res => {
-      console.log(res);
+      console.log('res', res);
+
+      for (const snippet of res){
+        setSnippets(prev => prev.push(snippet));
+        setSelectedSnippet({
+          id: snippet._id,
+          title: snippet.title,
+          comments: snippet.comments,
+          storedCode: snippet.storedCode,
+          tags: snippet.tags,
+          language: snippet.language
+        })
+      }
+
     })
     .catch( error => console.log('Get request failed', error))
   }
 
-  // do get request after each post request
+  // renderTags function
+  const renderTabs = () => {
+    const tabs = [];
 
-  useEffect(() => getSnippet(), []); // [] to be updated
+    for(let i = 0; i < snippets.length; i++){
+      tabs.push(<button className={styles.tab}>{snippets[i].title}</button>)
+    }
 
-  // addTag
-  const addTag = () => {
-
+    return tabs;
   }
 
+  // get data from backend at first page load
+  useEffect(() => getSnippet(), []);
+
     return (
-    <div className="sidebar">
-      <div className="logo"></div>
-      <div className="all_snippets">
-        <button className="all_snippets_btn">All Snippets</button>
+    <div className={styles.sidebar}>
+      <div className={styles.logo}></div>
+      <div className={styles.all_snippets}>
+        <button className={styles.tab}>All Snippets</button>
       </div>
-      <div className="tags">
-        <p className="tag_name">Tag Name:</p>
+      <div className={styles.tabs_display}>
+        <p className={styles.title}>Title:</p>
+        <div>
+          {renderTabs()}
+        </div>
       </div>
-      <SnippetDisplay />
-      <AddSnippet />
+         <div>
+          {snippets && <SnippetDisplay snippets={snippets} selectedSnippet={selectedSnippet} getSnippet={getSnippet} renderTags={renderTabs}/>
+          }
+         </div>
+        <AddSnippet snippets={snippets} selectedSnippet={selectedSnippet} getSnippet={getSnippet} renderTags={renderTabs}/>
     </div>
     );
 }
