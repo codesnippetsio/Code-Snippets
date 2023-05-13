1b8a35cab50726b7873c50a0a0c2e103ef1bdb44
Aalayah Olaes <aalayaholaes@Aalayahs-MacBook-Pro-2.local>
2023-05-13 16:41:27 -0400

    some questions for sun/mon to think about

--- client/src/components/SnippetDisplay/SnippetDisplay.jsx
+++ client/src/components/SnippetDisplay/SnippetDisplay.jsx
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 // import Snippet from 
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const SnippetDisplay = ({ indSnippet}) =>{
+const SnippetDisplay = ({ indSnippet }) =>{
     // indSnippet = this.props
     // create delete method using fetch request 
+
+    // create a state variable for each passed down state and the its setState function 
+    const [title, setTitle] = useState([]);
+
+
     const deleteSnippet = (_id) => {
         fetch (`/snippets?id=${_id}`, {
             method: 'DELETE',
         })
         .then((response) => {
             response.json()
-            return next()
         })
         .catch((err) => {
             return next({
@@ -23,8 +27,16 @@ const SnippetDisplay = ({ indSnippet}) =>{
     }
     // patch request
 
+    // pass down get request from display sidebar and call it after edit snippet
+    // populating object with updated state 
+
+    //patch request instead?? 
+    //have a button for after edit feature (save edit) 
+    // this will hold the state as 
+    // make patch request at the end, not individual patch request for each p tag thats edited 
+
     const editSnippet = (_id) => {
-        // const [oldState, updatedState] = React.useState([]);
+        const [oldState, setOldState] = React.useState([]);
         // create an object (eventually will hold the updated state)
             const updatedSnippet = {};
         // within fetch request (post)
@@ -35,7 +47,6 @@ const SnippetDisplay = ({ indSnippet}) =>{
             })
             .then((response) => {
                 response.json()
-                return next()
             })
             .catch((err) => {
                 return next({
@@ -56,7 +67,6 @@ const SnippetDisplay = ({ indSnippet}) =>{
     <div className="snippet_display">
 
       <div className="displayContainer"> 
-        <button className='copyButton' onClick={() => {copySnippet(this.props._id)}}> Copy Code</button>
         <p className="titleDisplay"> <span> Title: </span> {this.props.title}</p>
         <p className="languageDisplay"> <span> Language: </span> {this.props.language}</p>
         <p className="commentsDisplay"> <span> Comments: </span> {this.props.comments}</p>
@@ -76,13 +86,13 @@ const SnippetDisplay = ({ indSnippet}) =>{
         <button
             className="deleteButton"
             id={this.props._id}
-            onClick={() => {deleteSnippet(this.props._id); return window.location.reload(false)}}>
+            onClick={() => {deleteSnippet(this.props._id)}}>
             Delete Snippet 
       </button>
       <button
             className="editButton"
             id={this.props._id}
-            onClick={() => {editSnippet(this.props._id); return window.location.reload(false)}}>
+            onClick={() => {editSnippet(this.props._id)}}>
             Edit Snippet 
         </button>
       </div>
