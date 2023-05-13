5e83db22ef27e6dc55663d783470ba2252f8efb4
slimesown <plee50@binghamton.edu>
2023-05-13 12:30:17 -0400

    updated dev branch

--- server/server.js
+++ server/server.js
@@ -9,10 +9,10 @@ const snippetsRouter = require('./routes/snippets');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/snippets', snippetsRouter)
+app.use('/snippets', snippetsRouter);
 
 app.get('*', (req, res) => {
-  return res.status(404).send('404 NOT FOUND')
+  return res.status(404).send('404 NOT FOUND');
 });
 
 app.use((err, req, res, next) => {
@@ -26,6 +26,6 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(port, ()=> {
-  console.log(`Server listening on port ${port}...`)
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
 });
