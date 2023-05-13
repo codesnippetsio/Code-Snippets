7647a8ea27188b1f2f271cf5b52a4df50d2bbd93
slimesown <plee50@binghamton.edu>
2023-05-13 14:03:42 -0400

    Created a userModel

---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  tags: {
+    type: Object,
+    default: {},
+  },
+  languages: {
+    type: Object,
+    default: {},
+  },
+
+  snippets: {
+    type: [
+      {
+        type: Object,
+        title: { type: String, required: true },
+        comments: { type: String },
+        storedCode: { type: String },
+
+        tags: [String],
+        language: { type: String },
+      },
+    ],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
