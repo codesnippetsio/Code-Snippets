921d9f41b6c60b1be63060a374a604f3a40424fa
Aalayah Olaes <aalayaholaes@Aalayahs-MacBook-Pro-2.local>
2023-05-13 13:39:24 -0400

    set state

---
+++ client/src/containers/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+
+const Sidebar = () => {
+  const [state, setState] = useState({
+    snippets: [],
+    selectedSnippet: {
+      id: '',
+      title: '',
+      commets: '',
+      storedCode: '',
+      tags: [],
+      language: ''
+    },
+  });
+
+  // set up getSnippet func
+    // send get request to server
+    // setState with data
+
+
+    return (
+    <div className="sidebar">
+    </div>
+    );
+}
+
+export default Sidebar;
