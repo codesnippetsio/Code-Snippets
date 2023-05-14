c1431b21045e3773c005cb041c0a6ed3502778e7
Scott Brasko <scottbrasko@gmail.com>
2023-05-14 13:37:48 -0500

    completed TagInput component | still need to add styling

--- client/src/components/ui/TagInput/TagInput.jsx
+++ client/src/components/ui/TagInput/TagInput.jsx
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
-// import { render } from 'react-dom';
+import React, { useEffect } from 'react';
+import { TAGS } from '../../../data/data';
 import styles from './TagInput.module.scss';
 import { WithContext as ReactTags } from 'react-tag-input';
+import PropTypes from 'prop-types';
 
-const suggestions = COUNTRIES.map(country => {
+const suggestions = TAGS.map((tag) => {
   return {
-    id: country,
-    text: country
+    id: tag,
+    text: tag,
   };
 });
 
 const KeyCodes = {
   comma: 188,
-  enter: 13
+  enter: 13,
 };
 
 const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
-const App = () => {
-  const [tags, setTags] = React.useState([
-    { id: 'Thailand', text: 'Thailand' },
-    { id: 'India', text: 'India' },
-    { id: 'Vietnam', text: 'Vietnam' },
-    { id: 'Turkey', text: 'Turkey' }
-  ]);
+const TagInput = (props) => {
+  const initialTags = props.tags ? props.tags : [];
+  const [tags, setTags] = React.useState(initialTags);
 
-  const handleDelete = i => {
+  const handleDelete = (i) => {
     setTags(tags.filter((tag, index) => index !== i));
   };
 
-  const handleAddition = tag => {
+  const handleAddition = (tag) => {
     setTags([...tags, tag]);
   };
 
@@ -43,26 +40,34 @@ const App = () => {
     setTags(newTags);
   };
 
-  const handleTagClick = index => {
+  const handleTagClick = (index) => {
     console.log('The tag at index ' + index + ' was clicked');
   };
 
+  useEffect(() => {
+    if (props.onChange) props.onChange(tags);
+  }, [tags]);
+
   return (
-    <div className="app">
-      <h1> React Tags Example </h1>
-      <div>
-        <ReactTags
-          tags={tags}
-          suggestions={suggestions}
-          delimiters={delimiters}
-          handleDelete={handleDelete}
-          handleAddition={handleAddition}
-          handleDrag={handleDrag}
-          handleTagClick={handleTagClick}
-          inputFieldPosition="bottom"
-          autocomplete
-        />
-      </div>
+    <div>
+      <ReactTags
+        tags={tags}
+        suggestions={suggestions}
+        delimiters={delimiters}
+        handleDelete={handleDelete}
+        handleAddition={handleAddition}
+        handleDrag={handleDrag}
+        handleTagClick={handleTagClick}
+        inputFieldPosition='bottom'
+        autocomplete
+      />
     </div>
   );
 };
+
+TagInput.propTypes = {
+  onChange: PropTypes.func,
+  tags: PropTypes.array
+};
+
+export default TagInput;
