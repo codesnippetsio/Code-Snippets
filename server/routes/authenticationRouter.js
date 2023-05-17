9e3caa624dd8a48c10525a4c5c9fb67e0933b8a3
Wan Wang <wanwangbtb@gmail.com>
2023-05-17 15:24:09 -0400

    completed all functionality for front-end to interact with the back-end for authentication and connection with database

--- server/routes/authenticationRouter.js
+++ server/routes/authenticationRouter.js
@@ -10,17 +10,19 @@ require('dotenv').config();
 const secret = process.env.JWT_SECRET;
 
 router.post('/signup', authenticationController.signUp, (req, res) => {
-  return res
-    .status(201)
-    .json(res.locals.newUser);
+  return res.status(201).json(res.locals.newUser);
 });
 
-router.post('/login', passport.authenticate('local', {session: false}), (req, res) => {
-  const token = jwt.sign({ userId: req.user.id}, secret, {expiresIn: '1d'});
-  return res
-    .status(202)
-    .json({ token });
-});
+router.post(
+  '/login',
+  passport.authenticate('local', { session: false }),
+  (req, res) => {
+    const token = jwt.sign({ userId: req.user.id }, secret, {
+      expiresIn: '1d',
+    });
+    return res.status(202).json({ token });
+  }
+);
 
 router.get('/', authenticationController.getUserData, (req, res) => {
   res.status(200).json(res.locals.userData);
