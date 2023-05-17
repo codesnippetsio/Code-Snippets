1aeb84ba37262119fda712a3f86d213e36f4bba6
24juice22 <mike.james.nunn@gmail.com>
2023-05-17 10:25:22 -0400

    update Footer Buttons

--- client/src/components/SnippetDisplay/FooterButtons.jsx
+++ client/src/components/SnippetDisplay/FooterButtons.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './SnippetCard.module.scss';
 import { Button } from 'react-bootstrap';
 
-const FooterButtons = ({ deleteSnippet, setEditButtonState }) => {
+const FooterButtons = ({
+  deleteSnippet,
+  setEditButtonState,
+  selectedSnippet,
+}) => {
   return (
     <div className={styles.buttonDiv}>
       <Button
