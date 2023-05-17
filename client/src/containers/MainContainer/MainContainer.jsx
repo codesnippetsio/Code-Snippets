8f28815be5ca3c2b395756b7864a0d85095f6648
Wan Wang <wanwangbtb@gmail.com>
2023-05-17 14:11:41 -0400

    completed css styling for login, signup and main content, and built connection with authenrication process and database

--- client/src/containers/MainContainer/MainContainer.jsx
+++ client/src/containers/MainContainer/MainContainer.jsx
@@ -1,14 +1,12 @@
-
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar.jsx';
 import styles from './MainContainer.module.scss';
 import Login from '../../components/userStart/Login.jsx';
 import Signup from '../../components/userStart/Signup.jsx';
 
-const MainContainer = () =>{
-
+const MainContainer = () => {
   const [login, setLogin] = useState(false);
-  const [haveAccount,setHaveAccount] = useState(true);
+  const [haveAccount, setHaveAccount] = useState(true);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -17,7 +15,7 @@ const MainContainer = () =>{
     const passwordInputValue = document.getElementById('password').value;
     document.getElementById('password').value = '';
 
-    fetch('/authentication', {
+    fetch('/authentication/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -29,34 +27,63 @@ const MainContainer = () =>{
     })
       .then((result) => result.json())
       .then((result) => {
-        console.log('result is: ', result);
+        console.log('result from login request: ', result);
+        setLogin(true);
       })
       .catch((err) => {
         console.log(err);
       });
 
+    //todo: remove this after testing
     setLogin(true);
   };
-
+  //functino to handle showing the signup page
   const handleHaveAccount = () => setHaveAccount(false);
-  const handleSigned = () => setHaveAccount(true);
-  
 
-  return login ? 
-    (
-      <div className={styles.container}>
-        <Sidebar />
-      </div>
-    ) : haveAccount ?  (
-      <div className={styles.container}>
-        <Login handleLogin={handleLogin} handleHaveAccount={handleHaveAccount}/>
-      </div>
-    ) : (
-      <div className={styles.container}>
-        <Signup handleSigned={handleSigned} /> 
-      </div>
-    );
+  //function to handle sign-up if username was not already taken
+  const handleSigned = (e) => {
+    e.preventDefault();
+    const nameValue = document.getElementById('user').value;
+    document.getElementById('user').value = '';
+    const passwordValue = document.getElementById('psw').value;
+    document.getElementById('psw').value = '';
 
+    fetch('/authentication/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: nameValue,
+        password: passwordValue,
+      }),
+    })
+      .then((result) => result.json())
+      .then((result) => {
+        console.log('result from signup request: ', result);
+        setHaveAccount(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    //todo: remove this after testing
+    setHaveAccount(true);
+  };
+
+  return login ? (
+    <div className={styles.container}>
+      <Sidebar />
+    </div>
+  ) : haveAccount ? (
+    <div className={styles.container}>
+      <Login handleLogin={handleLogin} handleHaveAccount={handleHaveAccount} />
+    </div>
+  ) : (
+    <div className={styles.container}>
+      <Signup handleSigned={handleSigned} />
+    </div>
+  );
 };
 
 export default MainContainer;
