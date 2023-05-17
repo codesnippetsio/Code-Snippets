69409c451ee9ef9314cc97e4fa2a27bc10a95345
Joseph-Caballero <123486138+Joseph-Caballero@users.noreply.github.com>
2023-05-16 21:19:12 -0400

    added react router and login page

---
+++ client/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function(){
+  const [validUser, setValidUser] = useState(false);
+
+  const navigate = useNavigate();
+
+    useEffect(() => {
+    if (validUser) {
+      navigate('/');
+    }
+  }, [navigate, validUser]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const user = {}
+    user.username = e.target.elements.username.value;
+    user.password = e.target.elements.password.value;
+    console.log("Username is", user.username)
+    console.log("Password is", user.password)
+
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then((response) => {
+        if (!response.ok){
+          throw new Error('Failed to login')
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.status === 201){
+          setValidUser(true);
+        }
+      })
+      .catch(error => {console.log(error)})
+  }
+
+  return (
+    <div id="loginPage">
+      <div class="ProjectName">Code Snippets</div>
+      <div class="credentialBox">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="username" />
+          <input type="password" name="password" />
+          <button type ="submit">Login</button>
+          <button>Signup</button>
+        </form>
+      </div>
+    </div>
+  )
+}
--- client/index.js
+++ client/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import ErrorPage from './Error-Page.jsx';
 import './src/scss/App.scss';
+import Login from './Login.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+		errorElement: <ErrorPage />,
+	},
+]);
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
