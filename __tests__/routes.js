46fd371584073c5d64ce62657894d765bfb3972f
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-05-17 08:56:56 -0500

    Added lifecycle, setup methods to test suite

--- __tests__/routes.js
+++ __tests__/routes.js
@@ -1,26 +1,55 @@
 const request = require('supertest');
+const mongoose = require('mongoose');
 const Users = require('../server/models/userModel');
+const Snippets = require('../server/models/snippetModel');
+
+require('dotenv').config();
 
 const server = 'http://localhost:3000';
+const mongoURI = process.env.MONGO_URI;
 
 describe('Snippets route', () => {
+  let user_id;
+  let snippet_id;
+  const username = '__DummyData__';
+  const password = 'codesmith';
+
   describe('GET', () => {
-    //before all test:
-    //Create a new user in the db
-    //save _id in a variable
+    //before all GET test:
+    const snippet = {
+      title: 'FAKE DATA',
+      comments: 'FUN COMMENTS!',
+      storedCode: 'cry()',
+      tags: [1, 2, 3],
+      language: 'Klingon',
+    };
+    beforeAll(async () => {
+      await mongoose.connect(mongoURI);
 
-    //after all test:
-    //delete user
+      const user = await Users.create({ username, password });
+      user_id = user._id;
 
-    it('responds with 200 status and json', () => {
+      const fakeSnippet = await Snippets.create(snippet);
+      snippet_id = fakeSnippet._id;
+      user.snippets.push(fakeSnippet._id);
+      return user.save();
+    });
+
+    afterAll(async () => {
+      await Users.findByIdAndDelete(user_id);
+      await Snippets.findByIdAndDelete(snippet_id);
+
+      return await mongoose.connection.close();
+    });
+    xit('responds with 200 status and json', () => {
       return request(server)
-        .get('/snippets/?_id=6463eb52ab99bf89a84a3ebd')
+        .get(`/snippets/?_id=${user_id}`)
         .expect(200)
         .expect('Content-Type', 'application/json; charset=utf-8');
     });
-    it('responds with data that has keys: title, comments, storedCode, language', () => {
+    xit('responds with data that has keys: title, comments, storedCode, language', () => {
       return request(server)
-        .get('/snippets/?_id=6463eb52ab99bf89a84a3ebd')
+        .get(`/snippets/?_id=${user_id}`)
         .expect((res) => {
           if (!res.body[0].hasOwnProperty('title')) {
             throw new Error("Expected 'title' key!");
@@ -36,9 +65,9 @@ describe('Snippets route', () => {
           }
         });
     });
-    it('responds with data that has key, tags, and value of an array', () => {
+    xit('responds with data that has key, tags, and value of an array', () => {
       return request(server)
-        .get('/snippets/?_id=6463eb52ab99bf89a84a3ebd')
+        .get(`/snippets/?_id=${user_id}`)
         .expect((res) => {
           if (!res.body[0].hasOwnProperty('tags')) {
             throw new Error("Expected 'tags' key!");
