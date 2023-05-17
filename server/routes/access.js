ce40fc00b2ec2c5d0a8d8bbf0fdb07e2d1dec407
Julian Babon <babonjmc@gmail.com>
2023-05-17 12:03:57 -0400

    completed accessController

--- server/routes/access.js
+++ server/routes/access.js
@@ -2,11 +2,15 @@ const express = require('express');
 const accessController = require('../controllers/accessController');
 const router = express.Router();
 
-router.post('/login');
+router.post('/login',accessController.verifyUser, (req,res) => {
+  return res.redirect('/');
+});
 	
 
 
-router.post('/signup');
+router.post('/signup',accessController.createUser, (req,res) => {
+  return res.redirect('/login');
+});
 
 
 module.exports = router;
