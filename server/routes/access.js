69cd5cb8be8d2b70dfd5c85d33a4eee6a68c85e4
Julian Babon <babonjmc@gmail.com>
2023-05-17 14:13:06 -0400

    set up db test files

--- server/routes/access.js
+++ server/routes/access.js
@@ -3,13 +3,13 @@ const accessController = require('../controllers/accessController');
 const router = express.Router();
 
 router.post('/login',accessController.verifyUser, (req,res) => {
-  return res.redirect('/');
+  return res.redirect('/mockdb.html');
 });
 	
 
 
 router.post('/signup',accessController.createUser, (req,res) => {
-  return res.redirect('/login');
+  return res.redirect('/testlogin.html');
 });
 
 
