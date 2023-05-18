45e3a8b2fe60260480478f4a8efbf7831f9fb75b
elduke717 <spacematcha@proton.me>
2023-05-18 17:01:25 -0400

    add latest changes, tests won't work

--- __tests__/MainContainer.test.js
+++ __tests__/MainContainer.test.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from '@testing-library/react';
 import MainContainer from '../client/src/containers/MainContainer/MainContainer.jsx';
 import Sidebar from '../client/src/containers/Sidebar/Sidebar.jsx';
+
+
+import styles from '../client/src/components/SnippetDisplay/SnippetCard.module.scss';
+import { Card } from 'react-bootstrap';
+import DarkModeToggler from '../client/src/components/DarkModeToggler/DarkModeToggler.jsx';
+import FooterButtons from '../client/src/components/SnippetDisplay/FooterButtons.jsx';
+import Snippet from '../client/src/components/SnippetDisplay/Snippet.jsx';
 
 describe('Maincontainer', () => {
   it('should render the MainContainer component', () => {
--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,15 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
         '/__mocks__/fileMock.js',
-    '\\.(css|less)$': '/__mocks__/styleMock.js',
+    '\\.(s?css|less)$': '/__tests__/styleMock.js',
+
+/*moduleNameMapper: {
+  '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/test/jest/__mocks__/fileMock.js',
+  '\\.(css|less)$': '<rootDir>/test/jest/__mocks__/styleMock.js',
+}*/
+
+    
     //   '^react(.*)$': '/vendor/react-master$1',
     '^config$': '/configs/app-config.js',
   },
